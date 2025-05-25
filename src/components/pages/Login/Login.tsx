ccf32d0020affb9cea680de3cb0f96ad52123cd6
Buddhini-123 <buddhiniup123@gmail.com>
2025-05-25 16:43:27 +0530

    input fields changed

--- src/components/pages/Login/Login.tsx
+++ src/components/pages/Login/Login.tsx
@@ -50,7 +50,7 @@ const Login = () => {
               placeholder="Email Address"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="w-full px-4 py-3 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              className="w-full px-4 py-3 rounded-full bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
               required
               />
           </div>
@@ -61,7 +61,7 @@ const Login = () => {
               placeholder="Password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="w-full px-4 py-3 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              className="w-full px-4 py-3 rounded-full bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
               required
               />
           </div>
